f915c391215f3f31ca45c26ba43533a72d5a357c
Leo Hsu <leo@LeodeMacBook-Pro.local>
2023-09-10 17:08:24 +0800

    9/10

--- grind_Single_Number.c
+++ grind_Single_Number.c
@@ -7,17 +7,17 @@ int compare(const void *a, const void *b){
 int singleNumber(int* nums, int numsSize){
     if(numsSize == 1)
         return nums[0];
-
+    // Sorting
     qsort(nums, numsSize, sizeof(int), compare);
-
+    // First number is single
     if(nums[0] != nums[1])
         return nums[0];
 
     for(int i = 1; i < numsSize - 1; i++){
-        if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+        if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) // number is single
             return nums[i];
     }
-
+    // Last number is single
     return nums[numsSize - 1];
 }
 
